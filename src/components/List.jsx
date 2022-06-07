0b900c9847d15bccd5b3924466131cd65331d38f
deguchi37458 <deguchi37458@gmail.com>
2022-06-07 17:32:55 +0900

    update register

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Register from "./Register"
+// import Register from "./Register"
 
 export default class List extends React.Component {
   constructor(props){
@@ -15,6 +15,22 @@ export default class List extends React.Component {
     const onClickButton = () => {
       console.log("ok");
     }
+    const addQuestion = () => {
+      // 追加
+      this.state.question.push({
+        title: this.refs.newTitle.value,
+        option1: this.refs.newOptions1.value,
+        option2: this.refs.newOptions2.value
+      });
+      // 保存
+      this.setState({
+        question : this.state.question
+      });
+      // 初期化
+      this.refs.newTitle.value='';
+      this.refs.newOptions1.value='';
+      this.refs.newOptions2.value='';
+    }
     return (
       <>
         <ul>
@@ -28,7 +44,11 @@ export default class List extends React.Component {
             )
           })}
         </ul>
-        <Register />
+        <p>Register</p>
+        <input type="text" ref="newTitle"/>
+        <input type="text" ref="newOptions1"/>
+        <input type="text" ref="newOptions2"/>
+        <input type="button" value="追加" onClick={addQuestion} />
       </>
     );
   }
