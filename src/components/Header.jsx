19a5022dd56c64df010216b6cf2ff57453c7351d
deguchi <deguchi37458@gmail.com>
2022-06-08 00:32:48 +0900

    update title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Ultimate Two Choices
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
