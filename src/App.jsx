4d2880a24e5abf84e51e836fde5bf468de809f80
deguchi37458 <deguchi37458@gmail.com>
2022-06-07 18:38:03 +0900

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Header } from "./components/Header"
+import Home from "./components/Home"
 import List from "./components/List"
 
 export const App = () => {
   return (
     <>
-      <Header />
-      <List />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }; 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import Register from "./Register"
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Home</h1>
+      </>
+    );
+  }
+}
