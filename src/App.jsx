43a0d87d746a9a9bea0d1ca719bb78fb85856812
deguchi37458 <deguchi37458@gmail.com>
2022-06-08 16:27:03 +0900

    update App components

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,21 @@ import { Nav } from "./components/Nav"
 import "./assets/css/reset.min.css";
 import "./assets/css/App.scss";
 
-export const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Recents />} />
-          <Route path="/ranking" element={<Ranking />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/info" element={<Info />} />
-        </Routes>
-        <Nav />
-      </BrowserRouter>
-    </>
-  );
+export default class App extends React.Component{
+  render(){
+    return (
+      <>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Recents title="this.state"/>} />
+            <Route path="/ranking" element={<Ranking />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/info" element={<Info />} />
+          </Routes>
+          <Nav />
+        </BrowserRouter>
+      </>
+    );
+  }
 }; 
