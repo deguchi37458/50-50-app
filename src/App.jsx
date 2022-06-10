6a3f1d4f78ebde5efd0fd88f580be0f11d7d9f84
deguchi37458 <deguchi37458@gmail.com>
2022-06-10 10:58:19 +0900

    update Recents

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Header } from "./components/Header"
-import Recents from "./components/Recents"
+import { Recents } from "./components/Recents"
 import Ranking from "./components/Ranking"
 import Create from "./components/Create"
 import Info from "./components/Info"
 import { Nav } from "./components/Nav"
 
-import db from  "./firebase";
-import {query, collection, onSnapshot, orderBy} from "firebase/firestore"
-
 import "./assets/css/reset.min.css";
 
 export const App = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const postData = query(collection(db, "posts"), orderBy("question"));
-    onSnapshot(postData, (querySnapshot) => {
-      // console.log(querySnapshot.docs);
-      setPosts(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
-    });
-  }, []);
     return (
       <>
         <BrowserRouter>
           <Header />
           <Routes>
-            <Route exact path="/" element={<Recents posts={posts}/>} />
+            <Route exact path="/" element={<Recents />} />
             <Route path="/ranking" element={<Ranking />} />
             <Route path="/create" element={<Create />} />
             <Route path="/info" element={<Info />} />
--- src/components/Recents.jsx
+++ src/components/Recents.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from '@mui/material/Card';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 // import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 // import { Swiper, SwiperSlide } from 'swiper/react';
 
-// import db from  "../firebase";
-// import { collection, addDoc } from "firebase/firestore";
+import db from  "../firebase";
+import {query, collection, onSnapshot, orderBy} from "firebase/firestore"
 
-import 'swiper/css';
+// import 'swiper/css';
 
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
@@ -28,9 +28,18 @@ const buttonGroup = css`
   justify-content: center;
 `;
 
+export const Recents = () => {
 
-export default class Recents extends React.Component {
-  onClickButton = (e) =>{
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const postData = query(collection(db, "posts"), orderBy("question"));
+    onSnapshot(postData, (querySnapshot) => {
+      console.log(querySnapshot.docs);
+      setPosts(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
+  const onClickButton = (e) => {
     console.log(e.target.name);
     if(e.target.name === "answer1"){
       console.log("1");
@@ -38,35 +47,34 @@ export default class Recents extends React.Component {
       console.log("2");
     }
   }
-  render() {
-    return (
-      <>
-        {/* <Swiper
-          // install Swiper modules
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={50}
-          slidesPerView={1}
-          // navigation
-          // pagination={{ clickable: true }}
-          // scrollbar={{ draggable: true }}
-        > */}
-          {this.props.posts.map((post, i) => {
-            return (
-              // <SwiperSlide>
-                <Card>
-                  <div css={card}>
-                    <p css={title}>{post.question}</p>
-                    <ButtonGroup css={buttonGroup} disableElevation variant="contained">
-                      <Button name="answer1" onClick={this.onClickButton}>{post.answer1}</Button>
-                      <Button name="answer2" onClick={this.onClickButton}>{post.answer2}</Button>
-                    </ButtonGroup>
-                  </div>
-                </Card>
-              // </SwiperSlide>
-            )
-          })}
-        {/* </Swiper> */}
-      </>
-    );
-  }
+
+  return (
+    <>
+      {/* <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        // navigation
+        // pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+      > */}
+        {posts.map((post, i) => {
+          return (
+            // <SwiperSlide>
+              <Card>
+                <div css={card}>
+                  <p css={title}>{post.question}</p>
+                  <ButtonGroup css={buttonGroup} disableElevation variant="contained">
+                    <Button name="answer1" onClick={onClickButton}>{post.answer1}</Button>
+                    <Button name="answer2" onClick={onClickButton}>{post.answer2}</Button>
+                  </ButtonGroup>
+                </div>
+              </Card>
+            // </SwiperSlide>
+          )
+        })}
+      {/* </Swiper> */}
+    </>
+  ); 
 }
