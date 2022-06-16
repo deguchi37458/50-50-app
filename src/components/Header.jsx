84f72717bbe05bc286f673a64699f83e0593c4ef
deguchi37458 <deguchi37458@gmail.com>
2022-06-16 18:06:51 +0900

    add Drawer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,8 +7,21 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
 
 export const Header = () => {
+
+  const [open, setopen] = useState(false);
+  const toggleOpen=() => {
+      setopen(!open);
+  }
+
   return (
     <>
         <Box sx={{ flexGrow: 1 }} style={{height: '56px'}}>
@@ -19,6 +33,7 @@ export const Header = () => {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
+              onClick={toggleOpen}
             >
               <MenuIcon />
             </IconButton>
@@ -28,6 +43,22 @@ export const Header = () => {
             <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
+        <Drawer anchor='left' open={open} onClose={toggleOpen}>
+          <List>
+            <ListItem>
+              <ListItemIcon component={Link} to="/">
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" />
+            </ListItem>
+          </List>
+        </Drawer>
         </Box>
     </>
   )
